aea42b111b0f560b5318d08fd116437d1d0ff30a
ivioje <rhodaivioje@gmail.com>
2021-01-26 20:53:07 +0100

    created a custom hook

--- src/searchParam.js
+++ src/searchParam.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const Searchparams = () => {
   const [location, setLocation] = useState('Seattle, WA');
-  const [animal, setAnimal] = useState('dog');
-  const [breed, setBreed] = useState('');
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+  const [breed, BreedDropdown] = useDropdown('Breed', '',breeds);
 
   return (
     <div className='search-params'>
@@ -21,31 +22,8 @@ const Searchparams = () => {
             }}
           />
         </label>
-        <label htmlFor='animal'>
-          Animal
-          <select
-            id='animal'
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map(animal => (
-              <option value={animal} key={animal}>{animal}</option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select id='breed' value={breed} onChange={e => setBreed(e.target.value)}
-          onBlur={e => setBreed(e.target.value)} disabled={!breeds.length}
-          >
-          <option>All</option>
-          {breeds.map(breedString => (
-            <option value={breedString} key={breedString}>{breedString}</option>
-          ))}
-</select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
